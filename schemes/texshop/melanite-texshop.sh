2ce2bcc6d8627c6559e189b21962fc27d1aa5612
lpm11 <lpm11r@gmail.com>
2014-12-31 22:27:41 +0900

    Added texshop template & scheme.

---
+++ schemes/texshop/melanite-texshop.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+defaults write TeXShop background_R 0.1315524628925428
+defaults write TeXShop background_G 0.1514611456578815
+defaults write TeXShop background_B 0.1584508659361748
+
+defaults write TeXShop foreground_R 0.7675526591846704
+defaults write TeXShop foreground_G 0.7802913168283133
+defaults write TeXShop foreground_B 0.7848272996704031
+
+defaults write TeXShop commentred 0.456989727381416
+defaults write TeXShop commentgreen 0.4686302345468037
+defaults write TeXShop commentblue 0.47277872487402767
+
+defaults write TeXShop commandred 0.5152778241117596
+defaults write TeXShop commandgreen 0.7287980512108025
+defaults write TeXShop commandblue 0.3277610086866684
+
+defaults write TeXShop indexred 1.0
+defaults write TeXShop indexgreen 0.44605507023517926
+defaults write TeXShop indexblue 0.6444164367288312
+
+defaults write TeXShop markerred 0.0
+defaults write TeXShop markergreen 0.6473821768286941
+defaults write TeXShop markerblue 0.5877138942034104
+
+defaults write TeXShop insertionpoint_R 0.9329004893618941
+defaults write TeXShop insertionpoint_G 0.9460941572562335
+defaults write TeXShop insertionpoint_B 0.950788625916625
---
+++ templates/texshop/melanite-texshop.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+defaults write TeXShop background_R @melanite-bg1-rf
+defaults write TeXShop background_G @melanite-bg1-gf
+defaults write TeXShop background_B @melanite-bg1-bf
+
+defaults write TeXShop foreground_R @melanite-fg1-rf
+defaults write TeXShop foreground_G @melanite-fg1-gf
+defaults write TeXShop foreground_B @melanite-fg1-bf
+
+defaults write TeXShop commentred @melanite-fga-rf
+defaults write TeXShop commentgreen @melanite-fga-gf
+defaults write TeXShop commentblue @melanite-fga-bf
+
+defaults write TeXShop commandred @melanite-green-rf
+defaults write TeXShop commandgreen @melanite-green-gf
+defaults write TeXShop commandblue @melanite-green-bf
+
+defaults write TeXShop indexred @melanite-red-rf
+defaults write TeXShop indexgreen @melanite-red-gf
+defaults write TeXShop indexblue @melanite-red-bf
+
+defaults write TeXShop markerred @melanite-cyan-l60-rf
+defaults write TeXShop markergreen @melanite-cyan-l60-gf
+defaults write TeXShop markerblue @melanite-cyan-l60-bf
+
+defaults write TeXShop insertionpoint_R @melanite-fg-l95-rf
+defaults write TeXShop insertionpoint_G @melanite-fg-l95-gf
+defaults write TeXShop insertionpoint_B @melanite-fg-l95-bf
